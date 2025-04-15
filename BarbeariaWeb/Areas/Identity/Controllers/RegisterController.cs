89f3fe229318e2c3e559a4b67ced25c2b44c0336
patrickalvesz <patrickalvesgon@gmail.com>
2025-04-15 17:41:08 -0300

    Inserindo bibliotecas

---
+++ BarbeariaWeb/Areas/Identity/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BarbeariaWeb.Areas.Identity.Models;
+
+namespace BarbeariaWeb.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            UsuariosModel model = new();
+            model.listUsuarios = UsuariosModel.tUsuarios_GET();
+            return View(model);
+        }
+    }
+}
---
+++ BarbeariaWeb/Areas/Identity/Models/UsuariosModel.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using System.Data.SqlClient;
+namespace BarbeariaWeb.Areas.Identity.Models
+{
+    public class UsuariosModel
+    {
+        public List<UsuariosModel> listUsuarios { get; set; }
+        public int cId { get; set; }
+        public int cIdLoja { get; set; }
+        public string rNome { get; set; }
+        public string cTelefone { get; set; }
+        public string rEmail { get; set; }
+        public int cIdEmpregado { get; set; }
+        public int cProduto { get; set; }
+        public DateTime dtInicio { get; set; }
+        public DateTime dtFim { get; set; }
+        public DateTime dtCriacao { get; set; }
+        public SelectList comboEstadoCivil { get; set; }
+
+        private static readonly string connectionString = "Server=tcp:osfedido.database.windows.net,1433;Initial Catalog=BD_Barbearia;Persist Security Info=False;User ID=dev;Password=0105Goncalves;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public static List<UsuariosModel> tUsuarios_GET()
+        {
+            List<UsuariosModel> listUsuarios = new();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    listUsuarios = conn.Query<UsuariosModel>("pAgendamentos_GET", commandType: CommandType.StoredProcedure).ToList();
+                }
+
+                return listUsuarios;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return listUsuarios;
+            }
+        }
+
+    }
+    public class ClsCombo
+    {
+        public string cId { get; set; }
+        public string rStr { get; set; }
+        private static readonly string connectionString = "Server=tcp:osfedido.database.windows.net,1433;Initial Catalog=BD_Barbearia;Persist Security Info=False;User ID=dev;Password=0105Goncalves;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public static SelectList tCombo_GET(string operacao)
+        {
+            SelectList ret = null;
+            IEnumerable<ClsCombo> listCombos;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            DynamicParameters p = new DynamicParameters();
+            p.Add("@OPERACAO", operacao);
+            con = new SqlConnection(connectionString);
+
+            listCombos = con.Query<ClsCombo>("pCombo_GET", p, commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+            ret = new SelectList(listCombos, "cId", "rStr");
+
+            return ret;
+        }
+    }
+}

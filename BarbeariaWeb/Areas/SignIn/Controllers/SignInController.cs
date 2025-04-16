0d6595c55b1d24706d70e9c9123fe8aa9b025e9f
fgmatheus <125808812+fgmatheus@users.noreply.github.com>
2025-04-16 15:07:01 -0300

    Tela de Login e imagens

---
+++ BarbeariaWeb/Areas/SignIn/Controllers/SignInController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarbeariaWeb.Areas.SignIn.Controllers
+{
+    [Area("SignIn")]
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
